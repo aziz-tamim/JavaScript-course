ed9b2d4f36c780af458bd28902d4c595d7df1f57
abdul-aziz <tamimaziz2007@gmail.com>
2024-06-10 10:59:38 +0600

    update

---
+++ 115 Web Fetch API/promise.js
@@ -0,0 +1,8 @@
+const display = document.getElementById("display");
+function getData() {
+  fetch("http://127.0.0.1:5500/115%20Web%20Fetch%20API/data.txt")
+  .then((res) => res.text())    
+  .then(data => {
+    display.innerText = data;
+  })
+}
