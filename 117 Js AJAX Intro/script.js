97ad69f564ef023eb4f06161e61485e7ecba6f03
abdul-aziz <tamimaziz2007@gmail.com>
2024-06-25 16:57:09 +0600

    update

--- 117 Js AJAX Intro/script.js
+++ 117 Js AJAX Intro/script.js
@@ -1,15 +1,15 @@
 function loadData() {
-    // Create a new request
-    const xhr = new XMLHttpRequest();
+  // Create a new request
+  const xhr = new XMLHttpRequest();
 
-    // What to do when response arrives
-    xhr.onload = function() {
-        const container = document.getElementById('demo');
-        demo.innerHTML = xhr.responseText;
-    };
+  // What to do when response arrives
+  xhr.onload = function () {
+    const container = document.getElementById("demo");
+    demo.innerHTML = xhr.responseText;
+  };
 
-    // Prepare request - method: GET, POST, PUT, PATCH, DELETE, OPTIONS
-    xhr.open("GET", "./data/data.txt");
-    // Send request
-    xhr.send();
+  // Prepare request - method: GET, POST, PUT, PATCH, DELETE, OPTIONS
+  xhr.open("GET", "./data/data.txt");
+  // Send request
+  xhr.send();
 }
