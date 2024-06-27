46ab8e0002efd6c719471fa621843461c751fe77
abdul-aziz <tamimaziz2007@gmail.com>
2024-06-27 15:31:24 +0600

    update

--- 118 AJAX XMLHttp/script.js
+++ 118 AJAX XMLHttp/script.js
@@ -1,31 +1,31 @@
 function loadData(callbackF) {
-    // create a new request
-    const xhr = new XMLHttpRequest();
+  // create a new request
+  const xhr = new XMLHttpRequest();
 
-    // what to do when response arrives
-    xhr.onload = function() {
-        callbackF(this);
-    }
+  // what to do when response arrives
+  xhr.onload = function () {
+    callbackF(this);
+  };
 
-    // prepare request - methods: GET, POST, PUT, PATCH, DELETE, OPTION
-    xhr.open("GET", "./data/data.txt");
+  // prepare request - methods: GET, POST, PUT, PATCH, DELETE, OPTION
+  xhr.open("GET", "./data/data.txt");
 
-    // add a request header
-    // xhr.setRequestHeader("MY_GF, "JavaScript");
+  // add a request header
+  // xhr.setRequestHeader("MY_GF, "JavaScript");
 
-    // send request
-    xhr.send();
+  // send request
+  xhr.send();
 
-    // about
-    // xhr.about();
+  // about
+  // xhr.about();
 }
 
 function myCallback1(xhr) {
-    const container = document.getElementById("demo");
-    container.innerHTML = xhr.responseText;
+  const container = document.getElementById("demo");
+  container.innerHTML = xhr.responseText;
 }
 
 function myCallback2(xhr) {
-    const container = document.getElementById("demo2");
-    container.innerHTML = xhr.responseText;
+  const container = document.getElementById("demo2");
+  container.innerHTML = xhr.responseText;
 }
