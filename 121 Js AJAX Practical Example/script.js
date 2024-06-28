f3ece3eff6dad18a090c92f48453f70d3f0d4a01
abdul-aziz <tamimaziz2007@gmail.com>
2024-06-28 16:56:12 +0600

    Ajax practical example done

--- 121 Js AJAX Practical Example/script.js
+++ 121 Js AJAX Practical Example/script.js
@@ -1,52 +1,54 @@
 function sendRequest(method, url, data) {
-    
-    const promise = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-        xhr.onload = function () {
-            // handle application error
-            if (this.status >= 400) {
-                reject(`There was an application error and the response status is ${this.status}`);
-            }   else {
-                resolve(this.response);
-            }
-        };
-    
-        xhr.onerror = function () {
-            reject("There was an error");
-        };
+    xhr.onload = function () {
+      // handle application error
+      if (this.status >= 400) {
+        reject(
+          `There was an application error and the response status is ${this.status}`
+        );
+      } else {
+        resolve(this.response);
+      }
+    };
 
-        xhr.open(method, url);
-    
-        xhr.responseType = "json";
-        
-        xhr.send(data);
-    });
+    xhr.onerror = function () {
+      reject("There was an error");
+    };
 
-    return promise;
+    xhr.open(method, url);
+
+    xhr.responseType = "json";
+
+    xhr.send(data);
+  });
+
+  return promise;
 }
 
 function getData() {
-    sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/test").then((responseData) => {
-        console.log(responseData);
+  sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/test")
+    .then((responseData) => {
+      console.log(responseData);
     })
     .catch((err) => {
-        console.log(err);
-    })
+      console.log(err);
+    });
 }
 
 function sendData() {
-    sendRequest(
-        "POST",
-        "https://jsonplaceholder.typicode.com/posts",
-        JSON.stringify({
-            title: "foo",
-            body: "bar",
-            userId: 1,
-        })
-    ).then((responseData) => {
-        console.log(responseData);
-    });
+  sendRequest(
+    "POST",
+    "https://jsonplaceholder.typicode.com/posts",
+    JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    })
+  ).then((responseData) => {
+    console.log(responseData);
+  });
 }
 
 const getButton = document.getElementById("get");
