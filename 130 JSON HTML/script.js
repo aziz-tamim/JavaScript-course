ebb9450103243f77ae667f0781751102c09d0732
abdul-aziz <tamimaziz2007@gmail.com>
2024-07-04 16:39:59 +0600

    update

---
+++ 130 JSON HTML/script.js
@@ -0,0 +1,10 @@
+// async function change_myselect(sel) {
+//     // const dbParam = JSON.stringify({ table: sel, limit: 20});
+//     const response = await fetch("https://www.w3schools.com/js/json_demo_html_table.php", {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/x-www-form-urlencoded"
+//         },
+//         body: "x=" + JSON.stringify(dbParm),
+//     });
+// }
